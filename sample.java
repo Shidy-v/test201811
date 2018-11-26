6fd163559ac71d835b4f5ac6faad6fb0ccf85941
任意のユーザ名 <y.torihara@good-works.co.jp>
2018-11-26 15:55:36 +0900

    卍

--- sample.java
+++ sample.java
@@ -1,5 +1,10 @@
 public class sample{
 	public static void main (String [] args ){
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
 	}
 }
